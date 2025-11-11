31e2a9912d64aa9c4255043c03a6dfb81b389257
AL3X-TYLER-THE-CREATOR-FAN <galaxyhubunblockedgames@gmail.com>
2025-11-11 17:18:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,26 @@ function playSong(src, name, image) {
 volumeSlider.addEventListener('input', () => {
   player.volume = volumeSlider.value;
 });
+window.addEventListener('load', () => {
+  const player = document.getElementById('radioPlayer');
+  // try to play as soon as possible
+  player.play().catch(() => {
+    // browsers sometimes block autoplay; show a quick hint
+    const notice = document.createElement('div');
+    notice.textContent = 'click anywhere to start GOLF RADIO 🎵';
+    notice.style.position = 'fixed';
+    notice.style.top = '0';
+    notice.style.left = '0';
+    notice.style.right = '0';
+    notice.style.background = '#F2AECE';
+    notice.style.color = '#000';
+    notice.style.padding = '15px';
+    document.body.prepend(notice);
+
+    document.body.addEventListener('click', () => {
+      player.play();
+      notice.remove();
+    }, { once: true });
+  });
+});
+
